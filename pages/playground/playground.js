4e4490281ab9be1bf77a68aac5b27ae77679a306
SOL-MI <ovo10203@gmail.com>
2023-08-28 12:24:47 +0900

    Feat:particle 생성 수정

--- pages/playground/playground.js
+++ pages/playground/playground.js
@@ -2,9 +2,9 @@ const interactionBox = document.querySelector(".interaction");
 
 let i = 0;
 let change = 0;
-window.addEventListener('click',()=>{
+window.addEventListener("click", () => {
   i++;
-  switch(i%4){
+  switch (i % 4) {
     case 0:
       change = 0;
       break;
@@ -18,27 +18,26 @@ window.addEventListener('click',()=>{
       change = 300;
       break;
   }
-})
+});
 
-function createParticle (x, y) {
-
+function createParticle(x, y) {
   let size = Math.random() * 35;
-  
-  x -= (size / 2);
-  y -= (size / 2);
-  
-  let particle = document.createElement('div');
-  particle.classList.add('particle');
+
+  x -= size / 2;
+  y -= size / 2;
+
+  let particle = document.createElement("div");
+  particle.classList.add("particle");
   interactionBox.appendChild(particle);
-  
+
   TweenMax.set(particle, {
-    x: x, 
+    x: x,
     y: y,
     width: size,
     height: size,
-    background: bgColor(change)
+    background: bgColor(change),
   });
- 
+
   TweenMax.to(particle, Math.random() * 15 + 1, {
     x: x + (Math.random() - 0.5) * 200,
     y: y + (Math.random() - 0.5) * 200,
@@ -47,26 +46,36 @@ function createParticle (x, y) {
     ease: Power2.easeOut,
     onComplete: function () {
       interactionBox.removeChild(particle);
-    }
+    },
   });
 }
 
-const throttledCreateParticle = _.throttle(createParticle,15);
+const throttledCreateParticle = _.throttle(createParticle, 15);
 
-window.addEventListener('mousemove', function (e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  throttledCreateParticle(x, y);
-}, { passive: true });
+window.addEventListener(
+  "mousemove",
+  function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    throttledCreateParticle(x, y);
+  },
+  { passive: true }
+);
 
-interactionBox.addEventListener('touchmove', function (e) {
-  var x = e.touches[0].clientX;
-  var y = e.touches[0].clientY;
-  e.preventDefault();
-  throttledCreateParticle(x, y);
-}, { passive: true });
+interactionBox.addEventListener(
+  "touchmove",
+  function (e) {
+    //현재 터치 위치
+    for (let i = 0; i < e.touches.length; i++) {
+      let x = e.touches[i].clientX;
+      let y = e.touches[i].clientY;
+      e.preventDefault();
+      throttledCreateParticle(x, y);
+    }
+  },
+  { passive: true }
+);
 
-
-function bgColor (change) {
-  return `hsl(${Math.random() *90+change}, 50%, 70%)`
+function bgColor(change) {
+  return `hsl(${Math.random() * 90 + change}, 50%, 70%)`;
 }
